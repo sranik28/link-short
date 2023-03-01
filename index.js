fd38112201d0480cc585c08fec05f87d2c2f8eae
sranik28~ <anikshariarrahman@gmail.com>
2023-03-01 17:34:54 +0600

    short url copy added

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const input = document.getElementById('input-aira');
+const modalInput = document.getElementById('input-copy');
+const copyBtn = document.getElementById('copy-btn')
 const inputData = async (link) => {
     const url = `https://api.shrtco.de/v2/shorten?url=${link}`;
+    console.log(url)
     let res = await fetch(url);
     let data = await res.json();
-    // console.log(data)
+    console.log(data.result.short_link)
+    const modal = document.getElementById("my-modal-3").checked  = true;  
+    modalInput.value = data.result.short_link
 }
 const inputBtn = document.getElementById('input-button');
 inputBtn.onclick = ()=>{
     inputData(input.value)
     // console.log(input)
+}
+// copy btn
+copyBtn.onclick = () => {
+    navigator.clipboard.writeText(modalInput.value)
 }
